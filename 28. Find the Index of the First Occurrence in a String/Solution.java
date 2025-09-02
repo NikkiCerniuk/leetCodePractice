0c0845bc63dc8c1d3d0aee7a6c02b77bc142e55f
Nikki Cerniuk <ncerniuk@gmail.com>
2025-09-01 18:17:33 -0700

    Create Solution.java

---
+++ 28. Find the Index of the First Occurrence in a String/Solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int locationOfSubstring = haystack.indexOf(needle);
+        return locationOfSubstring;
+    }
+
+}
+
