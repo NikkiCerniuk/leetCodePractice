f606538bb8f06358db9d28fb7dc605e53c7df55b
Nikki Cerniuk <ncerniuk@gmail.com>
2025-09-01 18:23:19 -0700

    Update Solution.java

--- 28. Find the Index of the First Occurrence in a String/Solution.java
+++ 28. Find the Index of the First Occurrence in a String/Solution.java
@@ -3,6 +3,14 @@ class Solution {
         int locationOfSubstring = haystack.indexOf(needle);
         return locationOfSubstring;
     }
-
 }
 
+/*
+Time complexity: O(n*m) where:
+    - n is for the number of char in the haystack variable
+    - m is for the number of char in the needle variable 
+
+Space complexity: O(1) because:
+    - inputs dont count as extra space
+    - uses a fixed amount of memory, no matter how big the input gets 
+*/
